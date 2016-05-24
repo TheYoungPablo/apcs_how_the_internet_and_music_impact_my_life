9205a51b99ae81852b8aff5c974dfc05dca20d26
Bryan <bryan.c@bronxsoftware.org>
2016-05-24 09:06:26 -0400

    project 2.js
    This is my earsketch file

---
+++ javascript-code/script_name/author/description/project 2.js
@@ -0,0 +1,15 @@
+//		javascript code
+//		script_name:
+//
+//		author:
+//		description:
+//
+
+init(); 
+setTempo(100); 
+
+fitMedia(YG_WEST_COAST_HIP_HOP_BRASSES_1,1,3,21);
+fitMedia(HIPHOP_BASSSUB_005,2,1,21);
+fitMedia(Y17_DRUMPAD_1,3,2,21);
+fitMedia (Y03_TAMBOURINE_1,4,5,10);
+finish(); 
